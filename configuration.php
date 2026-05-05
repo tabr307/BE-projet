d2a43daaf4a6076d9f3980ee8dcba8bec1d23ccc
TheotimDlfg <delforgetheotim.pro@gmail.com>
2026-05-05 17:25:27 +0200

    Suppression du fichier de configuration par erreur

--- configuration.php
+++
@@ -1,8 +0,0 @@
-<?php
-// BE-PROJET/configuration.php
-
-define('DB_HOST', 'localhost');
-define('DB_PORT', 5432);
-define('DB_NAME', 'schema_reseau');
-define('DB_USER', 'postgres');
-define('DB_PASS', 'postgres'); //votre mot de passe 
